b9e5857f1d43e6848141713a7d470108563e8ad2
Edilaine Alves dos Santos <91744159+edilaine-as@users.noreply.github.com>
2022-10-13 23:12:08 -0300

    working in App.tsx

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,11 +6,12 @@ export interface TextProps{
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 //o children é porque o texto precisa ter algo dentro
 //o children era Text, mas tive que mudar para ReactNode poder receber a tag além do texto
 
-export function Text({ size = 'md', children, asChild }: TextProps){
+export function Text({ size = 'md', children, asChild, className }: TextProps){
     const Comp = asChild ? Slot : 'span' //caso eu receba uma tag, o asChild recebe true, e eu uso o Slot, caso asChild seja falso, eu uso o span
 
     return(
@@ -20,8 +21,9 @@ export function Text({ size = 'md', children, asChild }: TextProps){
                 'text-xs': size === 'sm',
                 'text-sn': size === 'md',
                 'text-md': size === 'lg',
-            }
-            
-            )}>{children}</Comp>
+            },
+            className,
+            )}
+        >{children}</Comp>
     )
 }
